ca0b600463ca311c0fb43845bd037e7e48ff74fe
Romain gore <romain22.rg@gmail.com>
2024-10-06 19:50:53 +0200

    navbar v2 + start carousel

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
-import './style.css'
+import './style.css';
+import 'flowbite';
+import Swiper from "swiper";
+
+var swiper = new Swiper(".progress-slide-carousel", {
+    loop: true,
+    fraction: true,
+    autoplay: {
+        delay: 1200,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".progress-slide-carousel .swiper-pagination",
+        type: "progressbar",
+    },
+});
 
 
 
