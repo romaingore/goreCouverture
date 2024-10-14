7e4eac31b236a4ecdb58dca86ddb66a63f8681da
romain goré <romaingore070204@gmail.com>
2024-10-14 15:44:15 +0200

    draft: sup carousel and add video header

--- main.js
+++ main.js
@@ -1,19 +1,6 @@
 import './style.css';
 import 'flowbite';
-import Swiper from "swiper";
 
-var swiper = new Swiper(".progress-slide-carousel", {
-    loop: true,
-    fraction: true,
-    autoplay: {
-        delay: 1200,
-        disableOnInteraction: false,
-    },
-    pagination: {
-        el: ".progress-slide-carousel .swiper-pagination",
-        type: "progressbar",
-    },
-});
 
 
 
